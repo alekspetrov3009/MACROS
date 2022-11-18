68f525855472a5717ff02722803df9ba0909a180
alekspetrov3009 <alekspetrov3009@gmail.com>
2022-11-18 15:20:06 +0300

    update

--- Заполнение служебной записки/C# version/SL Maker v2.0/WindowsFormsApp1/Form1.Designer.cs
+++ Заполнение служебной записки/C# version/SL Maker v2.0/WindowsFormsApp1/Form1.Designer.cs
@@ -33,67 +33,99 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 149);
+            this.button1.Location = new System.Drawing.Point(13, 199);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(229, 28);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Считать форматы";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 22);
+            this.label1.Location = new System.Drawing.Point(84, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(63, 51);
+            this.label2.Location = new System.Drawing.Point(84, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(63, 76);
+            this.label3.Location = new System.Drawing.Point(84, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(44, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "label3";
             // 
             // textBox1
             // 
             this.textBox1.AllowDrop = true;
-            this.textBox1.Location = new System.Drawing.Point(273, 22);
+            this.textBox1.Location = new System.Drawing.Point(364, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(319, 161);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(424, 197);
             this.textBox1.TabIndex = 6;
             this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
             this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 163);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить список";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 127);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 28);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Получить форматы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 195);
+            this.ClientSize = new System.Drawing.Size(805, 240);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -108,6 +140,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Заполнение служебной записки/C# version/SL Maker v2.0/WindowsFormsApp1/Form1.cs
+++ Заполнение служебной записки/C# version/SL Maker v2.0/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Reflection.Emit;
@@ -14,119 +15,104 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         private KompasObject kompas;
-
+        public List<string> paths = new List<string>();
+        private static IApplication _kompas7;
+        public List<string> Formats = new List<string>();
         public Form1()
         {
             InitializeComponent();
         }
 
+
+        private void StartKompas()
+        {
+            if (kompas == null)
+            {
+            #if __LIGHT_VERSION__
+			Type t = Type.GetTypeFromProgID("KOMPASLT.Application.5");
+            #else
+                Type t = Type.GetTypeFromProgID("KOMPAS.Application.5");
+            #endif
+                Console.WriteLine("Запускаем Компас в невидимом режиме..");
+                try { kompas = (KompasObject)Activator.CreateInstance(t); kompas.Visible = false; }
+                catch (Exception ee1) { MessageBox.Show(this, "Не могу запустить Компас!", "Сообщение"); }
+            }
+
+            else
+                MessageBox.Show(this, "Не найден активный объект", "Сообщение");
+        }
+
+
+        public void CloseKompas()
+        {
+            kompas.Quit();
+        }
+
+
+        public void openDrawing()
+        {
+
+            string progId = "KOMPAS.Application.5";
+            KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
+            ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
+            //используем API  - 7 версии
+            KompasAPI7._Application My7Komp = (_Application)kompas.ksGetApplication7();
+            IKompasAPIObject retw = My7Komp.ActiveDocument;
+            
+            for (int i = 0; i < paths.Count; i++)
+            {
+                IKompasDocument docOpen = My7Komp.Documents.Open(paths[i], false, true);
+                
+                // вызов метода чтения форматов
+                ReadDrawings();
+
+                Console.WriteLine(paths[i]);
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+
         private void button1_Click(object sender, EventArgs e)
         {
-            //Console.WriteLine(paths[0]);
-        }
-/*            var Formats = new List<string>();
-            // подключаемся к текущей сессии
-            string progId = "KOMPAS.Application.5";
             
-            KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
-            try
+            for (int i = 0; i < paths.Count; i++)
             {
-                kompas = (KompasObject)Marshal.GetActiveObject(progId);
+                Console.WriteLine(paths[i]);
             }
-            catch (Exception)
-            {
-                MessageBox.Show("Подключение к Компасу не прошло");
-            }
-            if (kompas != null)
-            {
+            
+        }
+          
 
-                kompas.Visible = true;
-                kompas.ActivateControllerAPI();
-                ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
-
-                // Узнаём количество листов в документе,
-                // соответственно можно организовать цикл по прохождениям по всем листам
-                // важно помнить что нумерация начинается с 1 - как и указана в штампе чертежа
-                int CountPages = doc.ksGetDocumentPagesCount();
-
-                //используем API  - 7 версии
-                KompasAPI7._Application My7Komp = (_Application)kompas.ksGetApplication7();
-                IKompasAPIObject retw = My7Komp.ActiveDocument;
-
-
-                // здесь искомый номер листа
-                int numb = 1;
-
-                // итерация по количеству листов в чертеже
-                while (numb <= CountPages)
-                {
-
-                    // правильней наверное было идти по шагам постепенно
-                    // сначало ActiveDocument, затем LayoutSheets, затем get_ItemByNumber
-                    // т.е. мы пропустили коллекцию листов - LayoutSheets - и сразу обратились по номеру
-                    ILayoutSheet MyLSheet = My7Komp.ActiveDocument.LayoutSheets.get_ItemByNumber(numb);
-
-
-                    // обращаемся к искомому объекту SheetFormat в котром и хранятся все данные о листе
-                    // формат, ориентация, кратность, высота, ширина,
-                    ISheetFormat ShFormat = MyLSheet.Format;
-                    //ISheetFormat count = MyLSheet.Count;
-
-
-                    // получаем формат в ввиде перечисления
-
-                    ksDocumentFormatEnum YesFormat = ShFormat.Format;
-
-                    // радуемся но не совсем,
-                    //label1.Text = YesFormat.ToString();//тк получим значение = ksFormatA3
-
-                    // для перевода в человеческий вид	
-                    int fgg = YesFormat.GetHashCode();
-                    label2.Text = "А" + fgg.ToString();
-                    // добавляем в список все форматы листов в чертеже
-                    Formats.Add(label2.Text.ToString());
-                    label3.Text = CountPages.ToString();
-                    numb++;
-
-                }
-                foreach (var item in Formats)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-        }*/
-        public void readDrawings()
+        public void ReadDrawings()
         {
-            var Formats = new List<string>();
+            //var Formats = new List<string>();
+
+            string progId = "KOMPAS.Application.5";
+            KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
+            ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
+
+            
             if (kompas == null)
             {
                 Type t = Type.GetTypeFromProgID("KOMPAS.Application.5");
                 kompas = (KompasObject)Activator.CreateInstance(t);
             }
-
-            if (kompas != null)
-            {
-                kompas.Visible = true;
-                kompas.ActivateControllerAPI();
-            }
-
-            ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
-
-            // Узнаём количество листов в документе,
-            // соответственно можно организовать цикл по прохождениям по всем листам
-            // важно помнить что нумерация начинается с 1 - как и указана в штампе чертежа
-            int CountPages = doc.ksGetDocumentPagesCount();
-
+            
             //используем API  - 7 версии
             KompasAPI7._Application My7Komp = (_Application)kompas.ksGetApplication7();
             IKompasAPIObject retw = My7Komp.ActiveDocument;
 
+            int CountPages = doc.ksGetDocumentPagesCount();
 
             // здесь искомый номер листа
             int numb = 1;
@@ -134,39 +120,33 @@ namespace WindowsFormsApp1
             // итерация по количеству листов в чертеже
             while (numb <= CountPages)
             {
-
-                // правильней наверное было идти по шагам постепенно
-                // сначало ActiveDocument, затем LayoutSheets, затем get_ItemByNumber
-                // т.е. мы пропустили коллекцию листов - LayoutSheets - и сразу обратились по номеру
                 ILayoutSheet MyLSheet = My7Komp.ActiveDocument.LayoutSheets.get_ItemByNumber(numb);
-
 
                 // обращаемся к искомому объекту SheetFormat в котром и хранятся все данные о листе
                 // формат, ориентация, кратность, высота, ширина,
                 ISheetFormat ShFormat = MyLSheet.Format;
-                //ISheetFormat count = MyLSheet.Count;
-
-
+                
                 // получаем формат в ввиде перечисления
-
                 ksDocumentFormatEnum YesFormat = ShFormat.Format;
-
-                // радуемся но не совсем,
-                //label1.Text = YesFormat.ToString();//тк получим значение = ksFormatA3
 
                 // для перевода в человеческий вид	
                 int fgg = YesFormat.GetHashCode();
-                label2.Text = "А" + fgg.ToString();
+                string drawingFormat = "А" + fgg.ToString();
                 // добавляем в список все форматы листов в чертеже
-                Formats.Add(label2.Text.ToString());
-                label3.Text = CountPages.ToString();
+                Formats.Add(drawingFormat.ToString());
+                string countOfPages = CountPages.ToString();
                 numb++;
 
+                Console.WriteLine(drawingFormat);
+                Console.WriteLine(countOfPages);
             }
+            
+            /*
             foreach (var item in Formats)
             {
                 Console.WriteLine(item);
             }
+            */
 
         }
 
@@ -184,7 +164,7 @@ namespace WindowsFormsApp1
 
         public void textBox1_DragDrop(object sender, DragEventArgs e)
         {
-            List<string> paths = new List<string>();
+            
             foreach (string obj in (string[])e.Data.GetData(DataFormats.FileDrop))
                 if (Directory.Exists(obj))
                     paths.AddRange(Directory.GetFiles(obj, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".cdw") || s.EndsWith(".spw")));
@@ -193,11 +173,31 @@ namespace WindowsFormsApp1
                     paths.Add(obj);
             foreach (string path in paths)
                 textBox1.Text += path + "\r\n";
-
-
         }
 
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Formats.Clear();
+            paths.Clear();
+            textBox1.Clear();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartKompas();
+            openDrawing();
+            CloseKompas();
+
+            /*
+            foreach (var item in Formats)
+            {
+                Console.WriteLine(item);
+            }
+            */
+            //ReadDrawings();
+        }
     }
     
 }

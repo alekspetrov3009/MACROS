2a540116f89f0bf997fa5fda2b9608dd347bd271
Aleksandr Petrov <59197302+alekspetrov3009@users.noreply.github.com>
2022-11-22 22:13:12 +0300

    update

--- Заполнение служебной записки/C# version/SL Maker v2.0/WindowsFormsApp1/Form1.cs
+++ Заполнение служебной записки/C# version/SL Maker v2.0/WindowsFormsApp1/Form1.cs
@@ -1,22 +1,13 @@
-﻿using KAPITypes;
-using Kompas6API5;
+﻿using Kompas6API5;
 using Kompas6Constants;
 using KompasAPI7;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Dynamic;
 using System.IO;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.WebRequestMethods;
 
 namespace WindowsFormsApp1
 {
@@ -26,7 +17,7 @@ namespace WindowsFormsApp1
         public List<string> paths = new List<string>();
         private static IApplication _kompas7;
         public List<string> Formats = new List<string>();
-        public string[] stampInfo;
+        List<string> countOfSheets = new List<string>();
         public Form1()
         {
             InitializeComponent();
@@ -37,11 +28,11 @@ namespace WindowsFormsApp1
         {
             if (kompas == null)
             {
-            #if __LIGHT_VERSION__
-			Type t = Type.GetTypeFromProgID("KOMPASLT.Application.5");
-            #else
+#if __LIGHT_VERSION__
+			            Type t = Type.GetTypeFromProgID("KOMPASLT.Application.5");
+#else
                 Type t = Type.GetTypeFromProgID("KOMPAS.Application.5");
-            #endif
+#endif
                 Console.WriteLine("Запускаем Компас в невидимом режиме..");
                 try { kompas = (KompasObject)Activator.CreateInstance(t); kompas.Visible = false; }
                 catch (Exception ee1) { MessageBox.Show(this, "Не могу запустить Компас!", "Сообщение"); }
@@ -51,26 +42,31 @@ namespace WindowsFormsApp1
                 MessageBox.Show(this, "Не найден активный объект", "Сообщение");
         }
 
+        public void InterfaceConnection()
+        {
+            string progId = "KOMPAS.Application.5";
+            KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
+            ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
+        }
+
 
         public void CloseKompas()
         {
             kompas.Quit();
         }
 
+
         public void OpenDrawing()
         {
-
-            string progId = "KOMPAS.Application.5";
-            KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
-            ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
+            InterfaceConnection();
             //используем API  - 7 версии
             KompasAPI7._Application My7Komp = (_Application)kompas.ksGetApplication7();
             IKompasAPIObject retw = My7Komp.ActiveDocument;
-            
+
             for (int i = 0; i < paths.Count; i++)
             {
                 IKompasDocument docOpen = My7Komp.Documents.Open(paths[i], false, true);
-                
+
                 // вызов метода чтения форматов
                 ReadDrawings();
 
@@ -82,54 +78,43 @@ namespace WindowsFormsApp1
 
         public void ReadShtamp()
         {
-            string progId = "KOMPAS.Application.5";
-            KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
             ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
+            KompasAPI7._Application My7Komp = (_Application)kompas.ksGetApplication7();
+            ILayoutSheet MyLSheet = My7Komp.ActiveDocument.LayoutSheets.get_ItemByNumber(1);
             ksStamp stamp = (ksStamp)doc.GetStamp();
-            if (stamp != null)
-            {
-                if (stamp.ksOpenStamp() == 1)
-                {
-                    stamp.ksOpenStamp();
-                    string[] stampInfo = stamp.ksGetStampColumnText(1);
-                    Console.WriteLine(stampInfo[0]);    
-                }
-            }
+            IStamp istamp = MyLSheet.Stamp;
+
+            IText naimenovanie = istamp.Text[1];
+            IText oboznachenie = istamp.Text[2];
+            Console.WriteLine(naimenovanie.Str);
+            Console.WriteLine(oboznachenie.Str);
         }
 
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-            for (int i = 0; i < paths.Count; i++)
-            {
-                Console.WriteLine(paths[i]);
-            }
-            
-        }
-          
 
         public void ReadDrawings()
         {
-            //var Formats = new List<string>();
+            
 
-            string progId = "KOMPAS.Application.5";
-            KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
+            InterfaceConnection();
+            //string progId = "KOMPAS.Application.5";
+            //KompasObject kompas = (KompasObject)Marshal.GetActiveObject(progId);
             ksDocument2D doc = (ksDocument2D)kompas.ActiveDocument2D();
 
-            
+
             if (kompas == null)
             {
                 Type t = Type.GetTypeFromProgID("KOMPAS.Application.5");
                 kompas = (KompasObject)Activator.CreateInstance(t);
             }
-            
+
             //используем API  - 7 версии
             KompasAPI7._Application My7Komp = (_Application)kompas.ksGetApplication7();
             //IKompasAPIObject retw = My7Komp.ActiveDocument;
 
+            // Количество листов в документе
             int CountPages = doc.ksGetDocumentPagesCount();
+
+            countOfSheets.Add(CountPages.ToString());
 
             // здесь искомый номер листа
             int numb = 1;
@@ -142,7 +127,7 @@ namespace WindowsFormsApp1
                 // обращаемся к искомому объекту SheetFormat в котром и хранятся все данные о листе
                 // формат, ориентация, кратность, высота, ширина,
                 ISheetFormat ShFormat = MyLSheet.Format;
-                
+
                 // получаем формат в ввиде перечисления
                 ksDocumentFormatEnum YesFormat = ShFormat.Format;
 
@@ -155,15 +140,22 @@ namespace WindowsFormsApp1
                 numb++;
 
                 Console.WriteLine(drawingFormat);
-                Console.WriteLine(countOfPages);
+
             }
-            
-            /*
-            foreach (var item in Formats)
+            Console.WriteLine(CountPages);
+            //Console.WriteLine(countOfSheets);
+            // чтение штампа
+            ReadShtamp();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < paths.Count; i++)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(paths[i]);
             }
-            */
 
         }
 
@@ -177,19 +169,19 @@ namespace WindowsFormsApp1
             }
         }
 
-       
 
         public void textBox1_DragDrop(object sender, DragEventArgs e)
         {
-            
+
             foreach (string obj in (string[])e.Data.GetData(DataFormats.FileDrop))
                 if (Directory.Exists(obj))
                     paths.AddRange(Directory.GetFiles(obj, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".cdw") || s.EndsWith(".spw")));
-
                 else
                     paths.Add(obj);
             foreach (string path in paths)
                 textBox1.Text += path + "\r\n";
+            int numbersOfSheets = paths.Count();
+            label4.Text = numbersOfSheets.ToString();
         }
 
 
@@ -198,57 +190,85 @@ namespace WindowsFormsApp1
             Formats.Clear();
             paths.Clear();
             textBox1.Clear();
-            
+            label4.Text = "0";
+
         }
 
-        public void labels()
-        {
-            int numbersOfSheets = paths.Count();
-            //string addedSheets =
-            //label4 = ToString().numbersOfSheets;
-        }
 
         private void button3_Click(object sender, EventArgs e)
         {
             StartKompas();
             OpenDrawing();
             CloseKompas();
-            Console.WriteLine(numberof)
 
-            
             foreach (var item in Formats)
             {
                 Console.WriteLine(item);
             }
-            foreach (var shtamps in stampInfo)
+
+            foreach (var sheets in countOfSheets)
             {
-                Console.WriteLine(shtamps);
+                Console.WriteLine(sheets);
             }
-            
+
             //ReadDrawings();
         }
 
-        
+
+        private void сhooseFolderButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogresult = folderBrowserDialog1.ShowDialog();
+            //Надпись выше окна контрола
+            folderBrowserDialog1.Description = "Выбор папки";
+            string folderName = "";
+            if (dialogresult == DialogResult.OK)
+            {
+                //Извлечение имени папки
+                folderName = folderBrowserDialog1.SelectedPath;
+            }
+            textBox2.Text = folderName;
+        }
+
+  //,dfgm,dflmgsmg
+
+
+
+        //protected void newdocument()
+        //{
+        //    var application = new Microsoft.Office.Interop.Excel.Application();
+        //    var workbook = application.Workbooks.add(template: geturi());
+        //    var worksheet = (Worksheet)workbook.sheets[1];
+        //}
+
+        //private string geturi()
+        //{
+        //    var resource = new { name = "служебная_записка_на_обработку_и_размножение_чертежей.xltx ", buff = resources.template };
+
+        //    var tempdirectory = path.getdirectoryname(path.gettempfilename());
+
+        //    var path = string.format("{0}\\{1}", tempdirectory, resource.name);
+
+        //    if (!file.exists(path) || file.readallbytes(path).length.equals(0))
+        //    {
+        //        var stream = new memorystream(resource.buff);
+
+        //        using (var file = new filestream(path, filemode.create))
+        //        {
+        //            var buffer = new byte[4096];
+        //            int bytesread;
+
+        //            while ((bytesread = stream.read(buffer, 0, buffer.length)) > 0)
+        //            {
+        //                file.write(buffer, 0, bytesread);
+        //            }
+        //        }
+        //    }
+
+        //    return path;
+        //}
+
+
+
     }
-    
+
 }
-
-
-        /*public void textBox1_DragEnter(object sender, DragEventArgs e)
-       {
-           if (e.Data.GetDataPresent(DataFormats.FileDrop))
-           {
-               string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
-               e.Effect = DragDropEffects.Copy;
-           }
-       }
-
-       public void textBox1_DragDrop(object sender, DragEventArgs e)
-       {
-           if (e.Data.GetDataPresent(DataFormats.FileDrop))
-           {
-               string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
-               foreach (string path in paths)
-                   textBox1.Text += path + "\r\n";*/
-
- 
